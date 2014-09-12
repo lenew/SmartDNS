8b1c43ad4986335ed261255fdf90e2e5a1b33b7d
lenew <wanglenew@gmail.com>
2014-09-12 13:56:02 +0800

    Update server.js
    support subnet mask

--- server.js
+++ server.js
@@ -45,12 +45,44 @@ function getDomain(buffer) {
     return question[0].name;
 }
 
+function ip2long(ip){
+    var components;
+
+    if(components = ip.match(/^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/))
+    {
+        var iplong = 0;
+        var power  = 1;
+        for(var i=4; i>=1; i-=1)
+        {
+            iplong += power * parseInt(components[i]);
+            power  *= 256;
+        }
+        return iplong;
+    }
+    else return -1;
+};
+
+function inSubNet(ip, subnet)
+{   
+    var mask, base_ip, long_ip = ip2long(ip);
+    if( (mask = subnet.match(/^(.*?)\/(\d{1,2})$/)) && ((base_ip=ip2long(mask[1])) >= 0) )
+    {
+        var freedom = Math.pow(2, 32 - parseInt(mask[2]));
+        return (long_ip > base_ip) && (long_ip < base_ip + freedom - 1);
+    }
+    else return false;
+};
 function isFakeIp(ip, list) {
     if(ip.length == 0) {
         return true;
     }
     for(var i in list) {
-        if(ip[0] == list[i]) {
+		var fi = list[i];
+		if(fi.indexOf("/")) {
+			return inSubNet(ip[0], list[i]);
+
+		}
+        else if(ip[0] == list[i]) {
             return true;
         }
     }
